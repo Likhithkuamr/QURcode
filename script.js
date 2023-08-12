7eba533a6d16342555c0109f34212f81cbf8fb92
jokeNquote <139613375+webbag1@users.noreply.github.com>
2023-08-12 20:15:42 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,61 @@
+  
+   const settingDiv = document.querySelector(".hero");
+   const hideButton = document.getElementById("hideButton");
+  
+    hideButton.addEventListener("click", function () {
+
+       if (settingDiv.style.height == "400px") {
+           settingDiv.style.height = "0";
+       } else {
+           settingDiv.style.height = "400px";
+
+       }
+
+   });
+  
+  
+  
   const textEL = document.querySelector('#data');
   const sizeEL = document.querySelector('#size');
   const logoEL = document.querySelector('#logo');
   const clearEL = document.querySelector('#clear');
   const marginEL = document.querySelector('#margin');
   const dotModeEL = document.querySelector('#dot');
+  const dotModecolorEL = document.querySelector('#colordot');
   const dotcolorEL1 = document.querySelector('#dot-color-1');
   const dotcolorEL2 = document.querySelector('#dot-color-2');
   const bgEL = document.querySelector('#bg-color');
   const dlEL = document.querySelector('#btn-dl');
 
+  const square = document.getElementById('1');
+  const dots = document.getElementById('2');
+  const rounded = document.getElementById('3');
+  const extrarounded = document.getElementById('4');
+  const classy = document.getElementById('5');
+  const classyrounded = document.getElementById('6');  
+  const fb = document.getElementById('11');
+  const insta = document.getElementById('22');
+  const yt = document.getElementById('33');
+  const tweet = document.getElementById('44');
+  const tele = document.getElementById('55');
+  const scan = document.getElementById('66');
+
+ 
+
+
   let op = {
-    width: 100, // You can set the default size here (e.g., 300px)
-    height: 100, // You can set the default size here (e.g., 300px)
+    width: 300,
+    height: 300,
     type: "png",
-    data: textEL.value,
-    image: "https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg",
+    data: "https://qurcode.netlify.app/",
+    margin: 6,
+    image: scan.src,
     dotsOptions: {
         color: "#4267b2",
-        type: "rounded",
+        type: "classy",
         gradient: {
             "type": "linear",
-            "colorStops": [ // Add the 'colorStops' field here
+            "colorStops": [ 
                 {
                     "offset": 0,
                     "color": "#000000"
@@ -34,11 +69,11 @@
     },
     backgroundOptions: {
         color: "#fff",
+    },
+    imageOptions: {
+        crossOrigin: "anonymous",
+        marginEL: 20
     }
-    // imageOptions: {
-    //     crossOrigin: "anonymous",
-    //     marginEL: 20
-    // }
 };
 
 
@@ -65,7 +100,10 @@
     op.dotsOptions.type = e.target.value;
     render();
   });
-
+  dotModecolorEL.addEventListener('change', e=>{
+    op.dotsOptions.gradient.type = e.target.value;
+    render();
+  });
   dotcolorEL1.addEventListener('input', e=>{
     op.dotsOptions.gradient.colorStops[0].color= e.target.value;
     render();
@@ -116,3 +154,69 @@
     qrCode.download({name: 'qr', extenstion:'svg'});
    });
 
+
+  square.addEventListener('click', e=>{
+    op.dotsOptions.type = "square";
+    render();
+  });
+
+  dots.addEventListener('click', e=>{
+    op.dotsOptions.type = "dots";
+    render();
+  });
+
+  rounded.addEventListener('click', e=>{
+    op.dotsOptions.type = "rounded";
+    render();
+  });
+
+  extrarounded.addEventListener('click', e=>{
+    op.dotsOptions.type = "extra-rounded";
+    render();
+  });
+
+  classy.addEventListener('click', e=>{
+    op.dotsOptions.type = "classy";
+    render();
+  });
+
+  classyrounded.addEventListener('click', e=>{
+    op.dotsOptions.type = "classy-rounded";
+    render();
+  });
+
+ fb.addEventListener('click', e=>{
+    const newImageUrl = fb.src; 
+            op.image = newImageUrl;
+    render();
+  });
+
+  yt.addEventListener('click', e=>{
+    const newImageUrl = yt.src; 
+            op.image = newImageUrl;
+    render();
+  });
+
+  insta.addEventListener('click', e=>{
+    const newImageUrl = insta.src; 
+            op.image = newImageUrl;
+    render();
+  });
+
+  tweet.addEventListener('click', e=>{
+    const newImageUrl = tweet.src; 
+            op.image = newImageUrl;
+    render();
+  });
+  
+  scan.addEventListener('click', e=>{
+    const newImageUrl = scan.src; 
+            op.image = newImageUrl;
+    render();
+  });
+
+  tele.addEventListener('click', e=>{
+    const newImageUrl = tele.src; 
+            op.image = newImageUrl;
+    render();
+  });   
